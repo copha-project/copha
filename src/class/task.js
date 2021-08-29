f4b0a40a4f4a625870d05c8ad43dd326f69aa7bd
aong <i@aong.cn>
2021-08-29 23:11:27 +0800

    task class tidy

--- src/class/task.js
+++ src/class/task.js
@@ -1,5 +1,4 @@
 const path = require('path')
-const fs = require('fs')
 const events = require('events')
 const Utils = require('uni-utils')
 const Base = require('./base')
@@ -17,13 +16,11 @@ class Task extends Base {
         this.#conf = conf
         this.#initValue()
         this.#initLogger()
-        // 初始化 web-driver
-        this.#loadWebDriver()
-        this.#loadJob()
-        // 初始化 用户自定义操作
-        this.#loadCustomCode()
-
-        this.#event = new events.EventEmitter()
+    }
+    static getPath(name,key){
+        if(!name) throw new Error(`task name cannot be empty`)
+        if(!this.AppTaskPathSet.hasOwnProperty(key)) throw new Error(`path about '${key}' not exist`)
+        return path.join(this.appSettings.DataPath,name,this.AppTaskPathSet[key])
     }
 
     //public 方法
@@ -99,115 +96,44 @@ class Task extends Base {
     }
     //public end
 
-
-    static getPath(name,key){
-        if(!name) throw new Error(`task name cannot be empty`)
-        if(!this.AppTaskPathSet.hasOwnProperty(key)) throw new Error(`path about '${key}' not exist`)
-        return path.join(this.appSettings.DataPath,name,this.AppTaskPathSet[key])
+    async init(){
+        // 初始化 web-driver
+        this.#loadWebDriver()
+        // 判断任务类型，加载任务模块
+        this.#loadJob()
+        // 初始化 用户自定义操作
+        this.#loadCustomCode()
+        // 初始化相关任务事件
+        this.#loadEvent()
     }
 
     async #startPrepare(){
+        this.init()
         this.#setExitHandle()
-        this.#event.on('taskCanStop',async ()=>{
-            this.log.warn('Ready to stop process, plaese waitting')
-            await this.#saveContext()
-            process.exit(0)
-        })
-        this.#setRunPid()
+        await this.#setRunPid()
+
+        await this.#driver?.init()
+        await this.#job?.init()
+
+        this.#job.setDriver(this.#driver)
+        this.#job.setCustom(this.#custom)
+        this.#job.setStorage(this.#storage)
     }
 
     async #test() {
         this.log.info(this.getMsg(6))
-        this.#driver.setTestState(true)
-        this.vTestState = true
         try {
-            await this.initDriver()
+            await this.#startPrepare()
             await this.#runBefore()
             await this.#job.runTest()
             await Utils.sleep(this.#conf.process.Test.WaitExitTime * 1000)
         } catch (error) {
-            this.log.err(`Run test err: ${error.message}`)
-        } finally {
-            await this.#clear()
-        }
-    }
-
-    async #start() {
-        this.log.info(this.getMsg(7,this.#name))
-        let hasErr = false
-        try {
-            // await this.#startPrepare()
-            await this.#driver?.init()
-            this.log.info('init Driver finished')
-            await this.#job?.init()
-
-            this.#job.setDriver(this.#driver)
-            this.#job.setCustom(this.#custom)
-
-            this.log.info('init job finished')
-            // await this.#loadState()
-            // await this.openUrl()
-            await this.#runBefore()
-            await this.#job?.run()
-            // this.vStartState = true
-            // await this.#listFetch()
-
-        } catch (error) {
-            this.log.err(`work start error: ${error}`)
-            hasErr = true
-        }
-        // await this.#checkNeedStop()
-        // 遇到错误退出程序，有可能的话重启进程
-        if (hasErr) {
-            try {
-                await this.#clear()
-                await this.#saveContext()
-                this.log.info(`check need to restart: ${this.#conf?.main?.alwaysRestart}`)
-                if (this.#conf?.main?.alwaysRestart) {
-                    await this.#restart()
-                }
-            } catch (error) {
-                //pass
-            }
-            return
-        }
-        await this.#execCode()
-        await this.#recover()
-        await this.#clear()
-        this.log.info('Task finished')
-    }
-
-    async #save(data, id) {
-        this.log.info(`save item data of ${id}`)
-        return this.storage.save({id,data})
-    }
-    async #find(id) {
-        return this.storage.findById(id)
-        // if (this.storage.type == "file") {
-        //     const file = path.join(this.getPath('saveDetailDataDir'), `${id}.json`)
-        //     return await Utils.checkFile(file) === true
-        // } else {
-        //     const res = await this.storage.query('detail', {
-        //         name: id
-        //     })
-        //     return res.length === 1
-        // }
-    }
-
-    // 可代理的方法
-    async #execCode(){
-        if(await Utils.checkFile(this.getPath('custom_exec_code')) !== true) throw new Error(this.getMsg(5))
-        this.log.info('start exec custom code')
-        const customCode = require(this.getPath('custom_exec_code'))
-        return customCode?.call(this)
-    }
-
-
-    async #clear() {
-        return this.#driver?.clear()
+            this.log.err(`Run test err: ${error}`)
+        } 
     }
 
     //interface of job
+
     async #runBefore() {
         if (this.#conf.process?.CustomStage?.RunBefore) {
             await this.#custom?.runBefore.call(this)
@@ -217,27 +143,48 @@ class Task extends Base {
         this.log.info('Runbefore in task finished')
     }
 
-    // self method
-    #setRunPid(){
-        fs.writeFileSync(this.getPath('pid'), `${process.pid}`)
-    }
-
-    #setExitHandle() {
-        let exitProcess = false // 防抖，多次按退出
-        const exitScript = async (args) => {
-            // 只有start需要执行特别退出流程
-            if (!this.vStartState) process.exit(0)
-            this.log.warn(`get exit signal by ${args}`)
-            if (!exitProcess) {
-                exitProcess = true
-                // 等待正在进行的任务，通过检查vNeedStop变量来判断是否需要暂停任务
-                this.log.warn('检测到关闭操作，通知业务暂停。')
-                this.vNeedStop = true
+    async #start() {
+        this.log.info(this.getMsg(7,this.#name))
+        try {
+            await this.#startPrepare()
+            await this.#runBefore()
+            await this.#job?.run()
+            await this.#execCode()
+        } catch (error) {
+            this.log.err(`Task start error: ${error}`)
+            // 遇到错误退出程序，有可能的话重启进程
+            await this.#saveContext()
+            this.log.info(`check need to restart: ${this.#conf?.main?.alwaysRestart}`)
+            if (this.#conf?.main?.alwaysRestart) {
+                // must delay some time to restart
+                this.#restart()
             }
         }
-        process.on('SIGINT', exitScript)
-        // process.on('SIGTERM', exitScript)
-        // process.on('SIGHUP',exitScript)
+        await this.#clear()
+        this.log.info('Task finished')
+    }
+
+    async #execCode(){
+        if(await Utils.checkFile(this.getPath('custom_exec_code')) !== true) throw new Error(this.getMsg(5))
+        this.log.info('start exec custom code')
+        const customCode = require(this.getPath('custom_exec_code'))
+        return customCode?.call(this)
+    }
+
+    async #clear() {
+        await this.#job?.clear()
+        await this.#driver?.clear()
+        // delete pid file
+        await this.#clearPid()
+    }
+
+    async #saveContext() {
+        await this.#job?.saveContext()
+    }
+
+    // self method
+    async #setRunPid(){
+        return Utils.saveFile(`${process.pid}`,this.getPath('pid'))
     }
 
     async #deleteLog() {
@@ -253,28 +200,43 @@ class Task extends Base {
         await Utils.rm(`${this.getPath('data_dir')}/detail/*`)
     }
 
+    #setExitHandle() {
+        let exitProcess = false // 防抖，多次按退出
+        const exitScript = async (args) => {
+            this.log.warn(`get exit signal by ${args}`)
+            if (!exitProcess) {
+                exitProcess = true
+                // 等待正在进行的任务，通过检查vNeedStop变量来判断是否需要暂停任务
+                this.log.warn('检测到关闭操作，通知业务暂停。')
+                this.vNeedStop = true
+            }
+        }
+        process.on('SIGINT', exitScript)
+        // process.on('SIGTERM', exitScript)
+        // process.on('SIGHUP',exitScript)
+    }
+
     #initValue() {
-        this.currentPage = 1
-        this.pages = 1
-        this.state = null
-
-        this.reworkPages = []
-        // 临时状态设置
-        this.vItemsErrIndex = 0
-
         // 外界发出关闭指令，内部发出需要停止信号，通知相关流程暂停运行，等待程序关闭
         this.vNeedStop = false
-        // 测试流程运行标志
-        this.vTestState = false
-        // 正式运行流程运行标志
-        this.vStartState = false
     }
+    
     #initLogger(){
         if (this.#conf){
             this.setLog({
                 'infoPath': this.getPath('info_log'),
                 'errPath': this.getPath('err_log')
             })
+        }
+    }
+
+    #initStorage() {
+        this.log.debug('Task: init storage')
+        try {
+            this.#storage = new Storage(this.#conf)
+            this.#storage.init()
+        } catch (e) {
+            throw new Error(`init storage error: ${e}`)
         }
     }
 
@@ -296,6 +258,7 @@ class Task extends Base {
             throw new Error(`can't load custom driver module : ${error.message}`)
         }
     }
+    
     #loadJob(){
         const jobName = this.constData.AppTaskTypeMap[this.#conf.main?.type] || 'empty'
         try {
@@ -305,6 +268,7 @@ class Task extends Base {
             throw new Error(`can't load job [${jobName}] : ${error}`)
         }
     }
+    
     #loadCustomCode() {
         try {
             this.#custom =  require(this.getPath('custom_over_write_code'))
@@ -313,62 +277,32 @@ class Task extends Base {
         }
     }
 
-    #getRestartCount() {
-        if (!this.state?.RestartCount) {
-            this.state.RestartCount = 1
-        } else {
-            this.state.RestartCount += 1
-        }
-        return this.state.RestartCount
+    #loadEvent(){
+        this.#event = new events.EventEmitter()
+        this.#event.on('taskCanStop',async ()=>{
+            this.log.warn('Ready to stop process, plaese waitting')
+            await this.#saveContext()
+            process.exit(0)
+        })
     }
 
-    #initStorage() {
-        this.log.debug('Task: init storage')
-        try {
-            const storage = new Storage(this.#conf)
-            storage.init()
-            return storage
-        } catch (e) {
-            throw new Error(`init storage error: ${e}`)
-        }
-    }
-    // 恢复任务状态
-    async #recover() {
-        return this.#job?.recover()
-    }
-    async #saveContext() {
-        await this.#job?.saveContext()
-        // save context state
-        if (!this.vTestState) await this.saveState()
-        // delete pid file
-        fs.writeFileSync(this.getPath('pid'), ``)
-    }
     async #restart(){
-        const count = this.getRestartCount()
+        const count = 0
         const waitSecond = 2 ** (parseInt(count) || 1)
         this.log.warn(`-----wait ${waitSecond}s Restart Process -----`)
         await Utils.sleep(waitSecond * 1000)
-        Utils.restartProcess()
-    }
-    async #checkNeedStop() {
-        if (this.vNeedStop) {
-            this.log.warn('检测到停止信号，业务暂停30s，等待程序停止')
-            this.#event.emit('taskCanStop')
-            await Utils.sleep(30000)
-            this.log.warn('任务等待停止超时，强制停止进程')
-            process.exit(1)
-        }
+        // Utils.restartProcess()
     }
 
     get storage(){
         if(!this.#storage){
-            this.#storage = this.#initStorage()
+            this.#initStorage()
         }
         return this.#storage
     }
 
     get Driver(){
-        return this.#driver?.Driver
+        return this.#driver?.DriverModule
     }
 
     get #name(){
