ccda4ddd8769817cd800a0b1c952fd21fe34db3b
aong <yxwzaxns@gmail.com>
2021-09-08 13:59:40 +0800

    update IsDev const

--- src/helper.js
+++ src/helper.js
@@ -1,4 +1,4 @@
-const IsDev = !process.env.NODE_ENV || process.env.NODE_ENV === "development"
+const IsDev = process.env.NODE_ENV && process.env.NODE_ENV === "development" || process.env.COPHA_DEBUG
 const IsPro = process.env.NODE_ENV === 'production'
 
 module.exports = {
