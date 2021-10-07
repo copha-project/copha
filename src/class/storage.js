b27b907a56177d3a6a481e15d956d68e58897782
aong <i@aong.cn>
2021-10-07 19:09:14 +0800

    add storage class

---
+++ src/class/storage.js
@@ -0,0 +1,37 @@
+const path = require('path')
+const Base = require('./base')
+const Task = require('./task')
+
+class Storage extends Base {
+    db = null
+    #taskConf = null
+    #conf = null
+    constructor(taskConf) {
+        super()
+        this.#taskConf = taskConf
+        this.#conf = taskConf?.Storage || {}
+        this.storageType = taskConf?.Storage?.Name
+    }
+    get conf(){
+        return this.#conf
+    }
+    get taskConf(){
+        return this.#taskConf
+    }
+
+    async init(){}
+    
+    async findById(id){}
+
+    async all(){}
+
+    async save(data){}
+
+    async query(where){}
+
+    querySync(where){}
+
+    async close(){}
+}
+
+module.exports = Storage
