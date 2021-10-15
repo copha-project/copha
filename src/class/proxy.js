97cec38143c36345a1ec866550ed136e771eb16b
aong <yxwzaxns@gmail.com>
2021-10-15 15:30:05 +0800

    getInstance();getProxy()

--- src/class/proxy.js
+++ src/class/proxy.js
@@ -2,8 +2,25 @@ const path = require('path')
 const Base = require('./base')
 
 class Proxy extends Base {
+    static #instance = null
     constructor() {
         super()
+    }
+    static getInstance(){
+        if(!this.#instance){
+			this.log.debug('new proxy instance')
+            this.#instance = new this
+        }else{
+			this.log.debug('reuse config')
+		}
+        return this.#instance
+    }
+
+    getProxy(index){
+        if(this.appSettings.Proxy?.List?.length < index){
+            throw new Error('not find delare proxy info')
+        }
+        return this.appSettings.Proxy.List[index]
     }
 }
 
