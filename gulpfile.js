056d6ecc78e00a0f4638de453ff4bccea7139bfc
yxwzaxns <yxwzaxns@gmail.com>
2021-11-17 23:54:12 +0800

    remove babel;gen d.ts

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,28 @@
 const gulp = require("gulp")
+const del = require('del')
 const ts = require("gulp-typescript")
+const merge = require('merge2')
 
 const paths = {
     resource: ["src/resource/**/*"],
 }
 
-const tsProject = ts.createProject("tsconfig.json")
+gulp.task('clean', function(){
+  return del('./dist', {force:true});
+});
 
 gulp.task("copy-resource-file", function () {
     return gulp.src(paths.resource).pipe(gulp.dest("dist/resource"))
 });
 
 gulp.task("compile-ts", function () {
-  return tsProject.src().pipe(tsProject()).js.pipe(gulp.dest("dist"))
+  const tsProject = ts.createProject("tsconfig.json")
+  const tsResult = tsProject.src().pipe(tsProject())
+
+  return merge([
+    tsResult.dts.pipe(gulp.dest('dist/types')),
+    tsResult.js.pipe(gulp.dest('dist/'))
+  ])
 })
 
-gulp.task("default", gulp.series(gulp.parallel("copy-resource-file","compile-ts")))
+gulp.task("default", gulp.series('clean',gulp.parallel("copy-resource-file","compile-ts")))
