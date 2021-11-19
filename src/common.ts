b373d30321206140b8edf140f3616342d9300d2a
aong <yxwzaxns@gmail.com>
2021-11-19 14:48:31 +0800

    export common class

--- src/common.ts
+++ src/common.ts
@@ -1,140 +1,138 @@
 const path = require('path')
 const fs = require('fs')
 
-const isDebug = typeof process.env.COPHA_DEBUG !== "undefined"
-const isDev = process.env.NODE_ENV !== "production"
+class Common {
+  static isDebug = typeof process.env.COPHA_DEBUG !== "undefined"
+  static isDev = process.env.NODE_ENV !== "production"
 
-function homedir () {
-  const env = process.env
-  const home = env.HOME
-  const user = env.LOGNAME || env.USER || env.LNAME || env.USERNAME
+  static homedir() {
+    const env = process.env
+    const home = env.HOME
+    const user = env.LOGNAME || env.USER || env.LNAME || env.USERNAME
 
-  if (process.platform === 'win32') {
-    return env.USERPROFILE || env.HOMEDRIVE + env.HOMEPATH || home || null
+    if (process.platform === 'win32') {
+      return env.USERPROFILE || env.HOMEDRIVE + env.HOMEPATH || home || null
+    }
+
+    if (process.platform === 'darwin') {
+      return home || (user ? '/Users/' + user : null)
+    }
+
+    if (process.platform === 'linux') {
+      return home || (process.getuid() === 0 ? '/root' : (user ? '/home/' + user : null))
+    }
+
+    return home || null
   }
 
-  if (process.platform === 'darwin') {
-    return home || (user ? '/Users/' + user : null)
+  static async zipDir(dirPath, outPath) {
+    const dirStat = await fs.promises.stat(dirPath)
+
+    if (!path.isAbsolute(outPath)) {
+      throw Error(`outPath [ ${outPath} ] not exist.`)
+    }
+
+    if (!dirStat.isDirectory()) {
+      throw Error('input path not is a dir')
+    }
+
+    return this.zip(dirPath, outPath)
   }
 
-  if (process.platform === 'linux') {
-    return home || (process.getuid() === 0 ? '/root' : (user ? '/home/' + user : null))
-  }
+  static zip(inPath, outPath) {
+    return new Promise((resolve, reject) => {
+      const archiver = require('archiver')
 
-  return home || null
-}
+      const output = fs.createWriteStream(outPath)
 
-async function zipDir(dirPath,outPath){
-  const dirStat = await fs.promises.stat(dirPath)
+      const archive = archiver('zip', {
+        zlib: {
+          level: 9
+        } // Sets the compression level.
+      })
 
-  if(!path.isAbsolute(outPath)){
-    throw Error(`outPath [ ${outPath} ] not exist.`)
-  }
+      archive.on('error', function (err) {
+        reject(err)
+      })
 
-  if(!dirStat.isDirectory()){
-    throw Error('input path not is a dir')
-  }
+      archive.pipe(output)
 
-  return zip(dirPath,outPath)
-}
+      output.on('close', function () {
+        resolve(archive.pointer())
+      })
 
-function zip (inPath,outPath) {
-  return new Promise((resolve,reject)=>{
-    const archiver = require('archiver')
+      // append files from a sub-directory and naming it `new-subdir` within the archive
+      archive.directory(inPath, false)
 
-    const output = fs.createWriteStream(outPath)
-
-    const archive = archiver('zip', {
-      zlib: { level: 9 } // Sets the compression level.
+      archive.finalize()
     })
 
-    archive.on('error', function(err) {
-      reject(err)
-    })
+    // archive.append(fs.createReadStream(file1), { name: 'file1.txt' })
 
-    archive.pipe(output)
+    // append a file from string
+    // archive.append('string cheese!', { name: 'file2.txt' });
 
-    output.on('close', function() {
-      resolve(archive.pointer())
-    })
+    // append a file from buffer
+    // const buffer3 = Buffer.from('buff it!');
+    // archive.append(buffer3, { name: 'file3.txt' });
+
+    // append a file
+    // archive.file('file1.txt', { name: 'file4.txt' });
 
     // append files from a sub-directory and naming it `new-subdir` within the archive
-    archive.directory(inPath, false)
+    // archive.directory('subdir/', 'new-subdir');
 
-    archive.finalize()
-  })
+    // append files from a sub-directory, putting its contents at the root of archive
+    // archive.directory('subdir/', false);
 
-  // archive.append(fs.createReadStream(file1), { name: 'file1.txt' })
+    // append files from a glob pattern
+    // archive.glob('file*.txt', {cwd:__dirname});
+  }
 
-  // append a file from string
-  // archive.append('string cheese!', { name: 'file2.txt' });
+  static cp(source, destination) {
+    const ncp = require('ncp').ncp
+    ncp.limit = 10
+    return new Promise((resolve, reject) => {
+      ncp(source, destination, function (err) {
+        if (err) {
+          return reject(err)
+        }
+        resolve(null)
+      })
+    })
+  }
 
-  // append a file from buffer
-  // const buffer3 = Buffer.from('buff it!');
-  // archive.append(buffer3, { name: 'file3.txt' });
-
-  // append a file
-  // archive.file('file1.txt', { name: 'file4.txt' });
-
-  // append files from a sub-directory and naming it `new-subdir` within the archive
-  // archive.directory('subdir/', 'new-subdir');
-
-  // append files from a sub-directory, putting its contents at the root of archive
-  // archive.directory('subdir/', false);
-
-  // append files from a glob pattern
-  // archive.glob('file*.txt', {cwd:__dirname});
-}
-
-function cp(source,destination){
-  const ncp = require('ncp').ncp
-  ncp.limit = 10
-  return new Promise((resolve,reject)=>{
-    ncp(source, destination, function (err) {
-      if (err) {
-        return reject(err)
-      }
-      resolve(null)
-     })
-  })
-}
-
-function domain(func, errHandle, cb){
+  static domain(func, errHandle, cb) {
     const d = require('domain').create()
 
     d.on('error', async error => {
-        await errHandle(error)
+      await errHandle(error)
     })
     // 无法保证异步报错出现在cb之前
-    d.run(async ()=>{
-        try {
-            await func()
-        } catch (error) {
-            await errHandle(error)
-        }
-        d.exit()
-        cb && await cb()
+    d.run(async () => {
+      try {
+        await func()
+      } catch (error) {
+        await errHandle(error)
+      }
+      d.exit()
+      cb && await cb()
     })
-}
-
-const loadPackageEnv = () => {
-  const packageDir = path.dirname(path.dirname(__dirname))
-  if(!process.env.NODE_PATH){
-      process.env.NODE_PATH = packageDir
-  }else{
-      throw new Error("NODE_PATH  has value!")
   }
-  require('module').Module._initPaths()
+
+  static loadPackageEnv() {
+    const packageDir = path.dirname(path.dirname(__dirname))
+    if (!process.env.NODE_PATH) {
+      process.env.NODE_PATH = packageDir
+    } else {
+      throw new Error("NODE_PATH  has value!")
+    }
+    require('module').Module._initPaths()
+  }
 }
 
-module.exports = {
-  isDebug,
-  isDev,
-  homedir,
-  zipDir,
-  cp,
-  domain,
-  loadPackageEnv
-}
+module.exports = Common
 
-export {}
+export {
+  Common
+}
