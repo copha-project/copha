3dbec8cd78acf9c8720992537caa151a70055fa6
aong <yxwzaxns@gmail.com>
2021-11-19 11:40:39 +0800

    update dev script

--- dev.env.sh
+++ dev.env.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
+# change your_data_dir_name use your real data
 # usage:  . ./dev.env.sh
 export COPHA_DEBUG=1
 export COPHA_DATA_PATH="${your_data_dir_name}"
-alias copha="./node_modules/.bin/babel-node -- ./bin/dev.js"
+alias copha="./node_modules/.bin/ts-node -- ./src/bin/dev.ts"
